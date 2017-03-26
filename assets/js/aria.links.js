0ef34e102e3395173df81878ca01ea8581cf0283
Scott O'Hara <scottaohara@users.noreply.github.com>
2017-03-26 13:35:43 -0400

    Update and rename a11y.links.js to aria.links.js

--- assets/js/aria.links.js
+++ assets/js/aria.links.js
@@ -1,37 +1,29 @@
 ;(function ( w, doc ) {
-
   //enable strict mode
   'use strict';
-
   // Local object for method references
-  var a11yLINKS = {};
+  // and define meta-data of for this script
+  var ARIAlinks = {};
+  ARIAlinks.NS      = 'ARIAlinks';
+  ARIAlinks.AUTHOR  = 'Scott O\'Hara';
+  ARIAlinks.VERION  = '0.1.1';
+  ARIAlinks.LICENSE = 'https://github.com/scottaohara/select-to-datalist/blob/master/LICENSE';
 
-  // Meta
-  a11yLINKS.NS      = "a11yLINKS";
-  a11yLINKS.AUTHOR  = "Scott O'Hara";
-  a11yLINKS.VERION  = "0.1.0";
-  a11yLINKS.LICENSE = "https://github.com/scottaohara/select-to-datalist/blob/master/LICENSE";
 
-  var widget        = doc.querySelectorAll('[data-action="aria-link"]');
-  var widgetCount   = widget.length;
-
-  var enterKey      = 13;
 
   /**
    * Create Link Instances
    */
-  a11yLINKS.create = function () {
-
+  ARIAlinks.create = function () {
     // setup / cache vars
+    var widget = doc.querySelectorAll('[data-action="aria-link"]');
     var self;
     var url;
     var i;
 
     // run through all instances of data-action="aria-link"
-    for ( i = 0; i < widgetCount; i++ ) {
-
+    for ( i = 0; i < widget.length; i++ ) {
       self = widget[i];
-
       self.setAttribute('tabindex', '0');
       self.setAttribute('role', 'link');
       self.classList.add('link');
@@ -44,56 +36,55 @@
 
       // on click, go to url
       self.addEventListener('click', function () {
-        var goto = this.getAttribute('data-href');
+        var linkTo = this.getAttribute('data-href');
 
         // if the 'link' has the data-blank attribute,
         // open the link in a new window.
         // if not, then open it in the existing window
         if ( this.hasAttribute('data-blank') ) {
-          window.open(goto);
+          window.open(linkTo);
         }
         else {
-          window.location.href = goto;
+          window.location.href = linkTo;
         }
-
       });
 
       // on keypress, run the keytrolls function
-      self.addEventListener('keypress', a11yLINKS.keytrolls);
+      self.addEventListener('keypress', ARIAlinks.keytrolls);
+    } // for(widget.length)
+  }; // ARIAlinks.create()
 
-    } // for(widgetCount)
-
-  }; // a11yLINKS.create()
 
 
   /**
    * Keyboard Controls for the 'Links'
    */
-  a11yLINKS.keytrolls = function ( e ) {
-
-    var keyCode = e.keyCode || e.which;
+  ARIAlinks.keytrolls = function ( e ) {
+    var enterKey = 13;
+    var keyCode  = e.keyCode || e.which;
 
     switch ( keyCode ) {
-
       case enterKey:
         e.preventDefault();
         e.target.click();
         break;
 
+      default:
+        break;
     } // switch
+  }; // ARIAlinks.keytrolls()
 
-  }; // a11yLINKS.keytrolls()
 
 
   /**
    * Initialize Buttons Functions
    */
-  a11yLINKS.init = function () {
+  ARIAlinks.init = function () {
+    ARIAlinks.create();
+  }; // ARIAlinks.init()
 
-    a11yLINKS.create();
 
-  }; // a11yLINKS.init()
 
-  a11yLINKS.init();
+  ARIAlinks.init();
 
 })( this, this.document );
