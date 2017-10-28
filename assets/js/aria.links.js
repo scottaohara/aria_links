68464e7e03e85adea7ede1566a68e471c0f31afd
Scott O'Hara <scottaohara@gmail.com>
2017-10-28 14:06:01 -0400

    v.1.0.0
    create css file for styles.
    add new aria-label for ‘links’ that open in a new window, that take link text and append “opens in a new window”

--- assets/js/aria.links.js
+++ assets/js/aria.links.js
@@ -1,98 +1,101 @@
 ;(function ( w, doc, undefined ) {
-  //enable strict mode
-  'use strict';
-  /**
-   * Local object for method references
-   * and define script meta-data
-   */
-  var ARIAlinks = {};
-  w.ARIAlinks   = ARIAlinks;
+	'use strict';
 
-  ARIAlinks.NS      = 'ARIAlinks';
-  ARIAlinks.AUTHOR  = 'Scott O\'Hara';
-  ARIAlinks.VERION  = '0.1.2';
-  ARIAlinks.LICENSE = 'https://github.com/scottaohara/select-to-datalist/blob/master/LICENSE';
+	/**
+	 * Local object for method references
+	 * and define script meta-data
+	 */
+	var ARIAlinks = {};
+	w.ARIAlinks   = ARIAlinks;
 
-
-
-  /**
-   * Create Link Instances
-   */
-  ARIAlinks.create = function () {
-    // setup / cache vars
-    var widget = doc.querySelectorAll('[data-action="aria-link"]');
-    var self;
-    var url;
-    var i;
-
-    // run through all instances of data-action="aria-link"
-    for ( i = 0; i < widget.length; i++ ) {
-      self = widget[i];
-      self.setAttribute('tabindex', '0');
-      self.setAttribute('role', 'link');
-      self.classList.add('link');
-
-      // get the child data-link to identify where this 'link' 
-      // should be sending people to.
-      if ( self.querySelector('[data-link]') ) {
-        url = self.querySelector('[data-link]').textContent;
-        self.setAttribute('data-href', url);
-        self.querySelector('[data-link]').remove();
-      }
-
-      // on click, go to url
-      self.addEventListener('click', function () {
-        var linkTo = this.getAttribute('data-href');
-
-        // if the 'link' has the data-blank attribute,
-        // open the link in a new window.
-        // if not, then open it in the existing window
-        if ( this.hasAttribute('data-blank') ) {
-          window.open(linkTo);
-        }
-        else {
-          window.location.href = linkTo;
-        }
-      });
-
-      // on keypress, run the keytrolls function
-      self.addEventListener('keypress', ARIAlinks.keytrolls);
-    } // for(widget.length)
-  }; // ARIAlinks.create()
+	ARIAlinks.NS      = 'ARIAlinks';
+	ARIAlinks.AUTHOR  = 'Scott O\'Hara';
+	ARIAlinks.VERION  = '1.0.0';
+	ARIAlinks.LICENSE = 'https://github.com/scottaohara/accessible-components/blob/master/LICENSE.md';
 
 
+	/**
+	 * Create Link Instances
+	 */
+	ARIAlinks.create = function () {
+		// setup / cache vars
+		var self;
+		var url;
+		var i;
 
-  /**
-   * Keyboard Controls for the 'Links'
-   */
-  ARIAlinks.keytrolls = function ( e ) {
-    var enterKey = 13;
-    var keyCode  = e.keyCode || e.which;
+		var widget = doc.querySelectorAll('[data-aria-link]');
 
-    switch ( keyCode ) {
-      case enterKey:
-        e.preventDefault();
-        e.target.click();
-        break;
+		// run through all instances of [data-aria-link]
+		for ( i = 0; i < widget.length; i++ ) {
+			self = widget[i];
+			self.setAttribute('tabindex', '0');
+			self.setAttribute('role', 'link');
+			self.classList.add('aria-link');
 
-      default:
-        break;
-    } // switch
-  }; // ARIAlinks.keytrolls()
+			// get the child data-link to identify where this 'link'
+			// should be sending people to.
+			if ( self.querySelector('[data-link]') ) {
+				url = self.querySelector('[data-link]').textContent;
+				self.setAttribute('data-href', url);
+				self.querySelector('[data-link]').remove();
+			}
+
+			if ( self.hasAttribute('data-blank') ) {
+				var getText = self.textContent;
+				self.setAttribute('aria-label', getText + ', opens in a new window.');
+			}
+
+			// on click, go to url
+			self.addEventListener('click', function () {
+				var linkTo = this.getAttribute('data-href');
+
+				// if the 'link' has the data-blank attribute,
+				// open the link in a new window.
+				// if not, then open it in the existing window
+				if ( this.hasAttribute('data-blank') ) {
+					window.open(linkTo);
+				}
+				else {
+					window.location.href = linkTo;
+				}
+			});
+
+			// on keypress, run the keytrolls function
+			self.addEventListener('keypress', ARIAlinks.keytrolls);
+		} // for(widget.length)
+	}; // ARIAlinks.create()
 
 
+	/**
+	 * Keyboard Controls for the 'Links'
+	 */
+	ARIAlinks.keytrolls = function ( e ) {
+		var enterKey = 13;
+		var keyCode  = e.keyCode || e.which;
 
-  /**
-   * Initialize Links Functions
-   * if expanding this script, place any other
-   * initialize functions within here.
-   */
-  ARIAlinks.init = function () {
-    ARIAlinks.create();
-  }; // ARIAlinks.init()
+		switch ( keyCode ) {
+			case enterKey:
+				e.preventDefault();
+				e.target.click();
+				break;
+
+			default:
+				break;
+		} // switch
+	}; // ARIAlinks.keytrolls()
 
 
-  // go go JavaScript
-  ARIAlinks.init();
+	/**
+	 * Initialize Links Functions
+	 * if expanding this script, place any other
+	 * initialize functions within here.
+	 */
+	ARIAlinks.init = function () {
+		ARIAlinks.create();
+	};
+
+
+	// go go JavaScript
+	ARIAlinks.init();
 
 })( window, document );
